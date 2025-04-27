7283d2bc3510f2f7099ff0c372863b6921a99e57
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-04-27 14:44:39 +0600

    start

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
+import LayOutOne from "./layouts/LayOutOne";
+import Home from "./pages/Home";
+
+const App = () => {
+  const myRouter = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<LayOutOne />}>
+          <Route index element ={<Home />}/>
+        </Route>
+      </Route>
+    )
+  );
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+};
+
+export default App;
