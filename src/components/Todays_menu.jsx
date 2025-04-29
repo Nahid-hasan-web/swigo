76ee2e6689d88ec7c2319c826775c9b396e61af3
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-04-29 19:31:01 +0600

    tadays menu work done

--- src/components/Todays_menu.jsx
+++ src/components/Todays_menu.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import CommonHead from './Common/CommonHead'
-
+import pasta from '../assets/images/pasta.png'
 const Todays_menu = () => {
   return (
     <section className='bg-[#F4FDE7] pt-[60px] pb-[393px] lg:pt-[100px] '>
         <div className="container">
             <CommonHead CommonHead={"Today's Menu"} />
             <div className='nahid_todays_menu_row flex gap-[30px] justify-center ronded-[10px] overflow-hidden'>
-                <div className="singelmenu_item   w-[270px] h-[351px] rounded-[10px] overflow-hidden shadow-[0px_8px_16px_-2px_rgba(0,_0,_0,_0.1)] "></div>
+                <div className="singelmenu_item   w-[270px] h-[351px] rounded-[10px]  overflow-hidden shadow-[0px_8px_16px_-2px_rgba(0,_0,_0,_0.1)] ">
+                    <div className='w-full h-[230px] overflow-hidden '>
+                        <img src={pasta} alt="pasta" />
+                    </div>
+                    <div className=' w-full h-[121px] bg-white pt-[23px] '>
+                        <h2 className='text-xl font-semibold font-poppins text-[#222222] text-center mb-[10px]'>Pasta</h2>
+                        <p className=' text-[16px] font-normal font-poppins text-[#666666] text-center'>Lorem ipsum dolor sit amet,
+                        dipiscing elit, sed</p>
+                    </div>
+                </div>
 
             </div>
         </div>
