16d4e7929af48f4ddabfb9d5ed71833a0d87ba5c
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-04-29 19:39:24 +0600

    add hover effect

--- src/components/Todays_menu.jsx
+++ src/components/Todays_menu.jsx
@@ -6,12 +6,13 @@ const Todays_menu = () => {
     <section className='bg-[#F4FDE7] pt-[60px] pb-[393px] lg:pt-[100px] '>
         <div className="container">
             <CommonHead CommonHead={"Today's Menu"} />
-            <div className='nahid_todays_menu_row flex gap-[30px] justify-center ronded-[10px] overflow-hidden'>
-                <div className="singelmenu_item   w-[270px] h-[351px] rounded-[10px]  overflow-hidden shadow-[0px_8px_16px_-2px_rgba(0,_0,_0,_0.1)] ">
-                    <div className='w-full h-[230px] overflow-hidden '>
+            <div className='nahid_todays_menu_row flex gap-[30px] justify-center ronded-[10px] overflow-hidden '>
+                <div className="singelmenu_item relative group   w-[270px] h-[351px] rounded-[10px]  overflow-hidden shadow-[0px_8px_16px_-2px_rgba(0,_0,_0,_0.1)] ">
+                    <div className='w-full overflow-hidden relative'>
                         <img src={pasta} alt="pasta" />
+                        <div className="topslae  px-[9px] py-[3px] bg-[#FE9F10] absolute top-0 left-0 text-[12px] font-poppins font-semibold text-[#fff] rounded-br-[10px]">TOP SALE</div>
                     </div>
-                    <div className=' w-full h-[121px] bg-white pt-[23px] '>
+                    <div className=' w-full h-[130px] bg-white pt-[23px] absolute bottom-[0] left-0 duration-[.7s] group-hover:bottom-[-100%] '>
                         <h2 className='text-xl font-semibold font-poppins text-[#222222] text-center mb-[10px]'>Pasta</h2>
                         <p className=' text-[16px] font-normal font-poppins text-[#666666] text-center'>Lorem ipsum dolor sit amet,
                         dipiscing elit, sed</p>
