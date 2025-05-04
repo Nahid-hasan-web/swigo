e7a49b20f1ac188a62d37e6c19049eb94d456f99
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-05-04 12:46:42 +0600

    product headline added

--- src/pages/AllProduct.jsx
+++ src/pages/AllProduct.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import BreadCrumb from "../components/Common/BreadCrumb";
 import CatHead from "../components/Common/CatHead";
+import { FaStar } from "react-icons/fa";
+import { FaMotorcycle } from "react-icons/fa6";
 
 const AllProduct = () => {
   return (
@@ -8,7 +10,7 @@ const AllProduct = () => {
       <BreadCrumb />
       <section className="bg-white pt-[100px] pb-[350px] ">
         <div className="container">
-          <div className="catagory_row flex  justify-between">
+          <div className="catagory_row flex  gap-[100px]">
             <div className="catagory_sideBar w-[270px]">
               <CatHead cathead={"Current Search"} />
               <div className="flex flex-wrap gap-3 mt-10">
@@ -22,19 +24,51 @@ const AllProduct = () => {
                   Pizza
                 </button>
               </div>
-                <CatHead cathead={"Refine By Categories"} />
-                <div className="flex items-center gap-5 mt-5">
-                  <input id="Pizza" className="w-[25px] h-[25px]" type="checkbox" />
-                  <label for='Pizza' className="text-lg font-normal font-poppins text-primary">Pizza</label>
-                </div>
-                <div className="flex items-center gap-5 mt-5">
-                  <input id="Hamburger" className="w-[25px] h-[25px]" type="checkbox" />
-                  <label for='Hamburger' className="text-lg font-normal font-poppins text-primary">Hamburger</label>
-                </div>
-                <div className="flex items-center gap-5 mt-5">
-                  <input id="Sandwich" className="w-[25px] h-[25px]" type="checkbox" />
-                  <label for='Sandwich' className="text-lg font-normal font-poppins text-primary">Sandwich</label>
-                </div>
+              <CatHead cathead={"Refine By Categories"} />
+              <div className="flex items-center gap-5 mt-5">
+                <input
+                  id="Pizza"
+                  className="w-[25px] h-[25px]"
+                  type="checkbox"
+                />
+                <label
+                  for="Pizza"
+                  className="text-lg font-normal font-poppins text-primary"
+                >
+                  Pizza
+                </label>
+              </div>
+              <div className="flex items-center gap-5 mt-5">
+                <input
+                  id="Hamburger"
+                  className="w-[25px] h-[25px]"
+                  type="checkbox"
+                />
+                <label
+                  for="Hamburger"
+                  className="text-lg font-normal font-poppins text-primary"
+                >
+                  Hamburger
+                </label>
+              </div>
+              <div className="flex items-center gap-5 mt-5">
+                <input
+                  id="Sandwich"
+                  className="w-[25px] h-[25px]"
+                  type="checkbox"
+                />
+                <label
+                  for="Sandwich"
+                  className="text-lg font-normal font-poppins text-primary"
+                >
+                  Sandwich
+                </label>
+              </div>
+            </div>
+            {/* ----------- Product singel card */}
+            <div className="product head mb-4 lg:mb-6  flex justify-between  w-full ">
+              <h2 className="text-xl font-semibold font-poppins text-black ">Search Results</h2>
+              <h3 className="text-[16px] font-medium font-poppins text-[#666666]">Search: 51,740 items</h3>
             </div>
           </div>
         </div>
